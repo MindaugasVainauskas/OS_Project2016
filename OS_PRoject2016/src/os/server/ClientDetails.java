62bb74c62bbcda9ad02493a02e32c126584d50ad
M.Vainauskas <mindelis1@gmail.com>
2017-01-05 13:46:16 +0000

    Added runner classes for both client and server. Added variables for client details in ClientDetails class and put in mutator methods for those variables.

--- OS_PRoject2016/src/os/server/ClientDetails.java
+++ OS_PRoject2016/src/os/server/ClientDetails.java
@@ -1,5 +1,56 @@
 package os.server;
 
 public class ClientDetails {
+	//variables to hold client details
+	private String name;
+	private String Address;
+	private int accNumber;
+	private String username;
+	private String password;
+	
+	public ClientDetails(){
+		
+	}
+
+	//getters and setters for variables
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public int getAccNumber() {
+		return accNumber;
+	}
+
+	public void setAccNumber(int accNumber) {
+		this.accNumber = accNumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
